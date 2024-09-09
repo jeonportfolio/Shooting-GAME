d64eec164e2ece480431e5a0db318066e2473e29
jeons <etoile929@naver.com>
2024-09-09 12:02:17 +0900

    우주선 움직이기

--- Readme.md
+++ Readme.md
@@ -1,8 +1,24 @@
-# JAVASCRIPT 슈팅게임 
+# JAVASCRIPT 슈팅게임 🎮🎮🎮<br/><br/>
 
 ## 캔버스 세팅
 
-◎ ctx를 활용해 2d 캔버스 설정 -> html에 연동
-◎ `https://icons8.com/` 아이콘 사용
-◎ render 함수를 통해 UI에 이미지 적용 -> 지속적인 render로 애니메이션 효과(requestAnimationFrame)
-◎ 우주선 좌표 -> 계속적으로 변경됨 (가로 200 - 30 [절반기준])(높이 700 - 60)
+◎ ctx를 활용해 2d 캔버스 설정 -> html에 연동<br/>
+◎ `https://icons8.com/` 아이콘 사용<br/>
+◎ render 함수를 통해 UI에 이미지 적용 -> 지속적인 render로 애니메이션 효과(requestAnimationFrame)<br/>
+◎ 우주선 좌표 -> 계속적으로 변경됨 (가로 200 - 30 [절반기준])(높이 700 - 60)<br/>
+
+
+## 우주선 위치 변경 
+
+◎ 방향키를 누르면 우주선의 x,y좌표가 바뀌고 다시 render<br/>
+◎ addEventListener에서 keycode의 입력값을 받고 키보드의 클릭 유무 판단 <br/>
+◎ 우주선이 캔버스 밖으로 나가지 않게 하는 것(0~400px) 범위<br/>
+◎ 끝지점은 우주선의 가로크기 canvas.width에서 60px을 빼준다
+
+## 총알 발사
+
+◎ 스페이스 바를 누르면 발사<br/>
+◎ 총알은 위로 발사됨 -> y의 좌표 값이 줄어든다<br/>
+◎ 총알은 여러번 발사될 수 있음 <br/>
+◎ 모든 총알은 x,y좌표가 있음 <br/>
+◎ 배열로 저장 후 계속 render
--- main.js
+++ main.js
@@ -33,15 +33,54 @@ function loadImage() {
 }
 
 
+// 키보드의 입출력 값 변경 
+let keysDown = {};
+
+function setupKeyboardListener (){
+    document.addEventListener("keydown", function(event){
+        keysDown[event.keyCode] = true;
+        console.log("키다운 객체",keysDown);
+    });
+
+    document.addEventListener("keyup", function(event){
+        delete keysDown[event.keyCode]
+        console.log("버튼 클릭후", keysDown);
+    })
+}
+
+//우주선의 위치 변경 
+
+function update() {
+    //오른쪽 
+    if(39 in keysDown){
+        spaceshipX +=5;
+    }
+
+    //왼쪽
+    if(37 in keysDown){
+        spaceshipX -=5;
+    }
+
+    if(spaceshipX <= 0){
+        spaceshipX=0;
+    }
+    if(spaceshipX >= canvas.width - 60){
+        spaceshipX= canvas.width -60;
+    }
+}
+
+
 function render() {
     ctx.drawImage(backgroundImage, 0, 0,canvas.width, canvas.height);
     ctx.drawImage(spaceshipImage,spaceshipX,spaceshipY)
 }
 
 function main() {
+    update();
     render();
     requestAnimationFrame(main);
 }
 
 loadImage();
+setupKeyboardListener();
 main();
